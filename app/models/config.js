cbbcd9a0adc31c98de0a55c59e4c5c9c0dd70739
Jordan Hayashi <jhhayashi@gmail.com>
2016-12-18 00:25:27 -0500

    Pipe POST /users api endpoint through reverse proxy

--- app/models/config.js
+++ app/models/config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    port: 3001
+    port: 3001,
+
+    // location of backend, do not include trailing /
+    apiUrl: 'http://localhost:3000',
 };
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const request = require('request');
+const config = require('../app/models/config');
 
 router.get('/', (req, res, next) => {
     return res.render('index');
 });
 
 router.post('/', (req, res, next) => {
-    res.json(req.body);
+    request.post(config.apiUrl + '/users', {form: req.body}).pipe(res);
 });
 
 module.exports = router;
