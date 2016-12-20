8697a992c0e89bb1fdcff68d5d8aace5a8cb4153
Jordan Hayashi <jhhayashi@gmail.com>
2016-12-19 21:22:57 -0800

    Add /logout endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const request = require('request');
 const config = require('../app/models/config');
+const auth = require('./auth');
 
 router.get('/', (req, res, next) => {
     return res.render('index', {providers: config.providers});
 });
-
 router.post('/', (req, res, next) => {
     request.post(config.apiUrl + '/users', {form: req.body}).pipe(res);
 });
@@ -14,9 +14,12 @@ router.post('/', (req, res, next) => {
 router.get('/login', (req, res, next) => {
     return res.render('login');
 });
-
 router.post('/login', (req, res, next) => {
     request.post(config.apiUrl + '/auth/token', {form: req.body}).pipe(res);
+});
+
+router.all('/logout', (req, res, next) => {
+    return res.render('logout');
 });
 
 module.exports = router;
