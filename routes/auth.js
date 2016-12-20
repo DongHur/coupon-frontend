f701ec3a2688f42547de53ae483846ca47442319
Jordan Hayashi <jhhayashi@gmail.com>
2016-12-19 21:24:07 -0800

    Redirect validator to /logout on invalid token

--- routes/auth.js
+++ routes/auth.js
@@ -12,20 +12,20 @@ exports.superAdminRequired = (req, res, next) => {
 function validateToken(req, res, next, c) {
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-    if (!token) return res.redirect('/login');
+    if (!token) return res.redirect('/logout');
 
     try {
         var decoded = jwt.decode(token, config.secret);
     } catch(err) {
-        return res.redirect('/login');
+        return res.redirect('/logout');
     }
 
     if (c.adminRequired && !decoded.isAdmin && !decoded.isSuperAdmin)
-        return res.redirect('/login');
+        return res.redirect('/logout');
     if (c.superAdminRequired && !decoded.isSuperAdmin)
-        return res.redirect('/login');
+        return res.redirect('/logout');
 
-    if (!decoded.id) return res.redirect('/login');
+    if (!decoded.id) return res.redirect('/logout');
 
     req.user = decoded;
     next();
