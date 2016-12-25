bc042603af2da7f26db113d4a9118ef88b98624c
Jordan Hayashi <jhhayashi@gmail.com>
2016-12-25 14:51:03 -0800

    Migrate from jade to pug

--- coupon-frontend.js
+++ coupon-frontend.js
@@ -1,18 +1,18 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var config = require('./app/models/config');
-var routes = require('./routes/index');
+const config = require('./app/models/config');
+const routes = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app/views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'app', 'views'));
+app.set('view engine', 'pug');
 
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
@@ -21,13 +21,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // log requests
-if (app.get('env') === 'development')
-    app.use(logger('dev'));
+if (app.get('env') === 'development') app.use(logger('dev'));
 
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -35,15 +34,13 @@ app.use(function(req, res, next) {
 
 // development error handler
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use((err, req, res, next) => {
         if (err.status !== 404) console.log(err);
         res.status(err.status || 500).send();
     });
 }
 
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500).send();
-});
+app.use((err, req, res, next) => res.status(err.status || 500).send());
 
 var server = app.listen(config.port);
 console.log('Listening at http://localhost:%s in %s mode',
